b03693946525faa1aaae4768f56d78ef7dfef091
Zámbó Sándor Dávid <david@dcmf.hu>
2018-01-30 09:11:50 +0100

    Updated ip visualization to handle ipv4 addresses

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const port = process.env.PORT || 8000;
 app.get('/', (req, res) =>{
   const userAgent = req.headers['user-agent'];
   const regex = /\((.+)\)/g;
+  const ip = req.ip;
+ if (ip.substr(0,7) == "::ffff:")
+	ip = ip.substr(7);
   res.json({
-    yourIP: req.ip,
+    yourIP: ip,
     yourLanguage: req.headers['accept-language'].split(',')[0],
     yourSystem: regex.exec(userAgent)[1],
     codedBy: 'https://www.dcmf.hu'
